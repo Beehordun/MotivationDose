4269e5cac3fd6813c669114ff9e2ccdb15575c08
Biodun <talk2olatejuemmanuel@gmail.com>
2020-07-22 21:20:17 +0100

    Set up project, dependencies, static analysis tools and circle ci

---
+++ app/src/main/java/com/biodun/mindvalley/features/channel/ui/ChannelActivity.kt
@@ -0,0 +1,12 @@
+package com.biodun.mindvalley.features.channel.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.biodun.mindvalley.R
+
+class ChannelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_channel)
+    }
+}
